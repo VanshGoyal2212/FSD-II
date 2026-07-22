45374164898ff68ec53ad7bad9ef84f159e92801
VanshGoyal212 <vansh.25b01010052@abes.ac.in>
2026-07-22 11:08:35 +0530

    Day1

---
+++ Day1.js
@@ -0,0 +1,39 @@
+console.log("Hello, World!");
+
+var name="vansh";
+
+console.log("My name is " + name);
+console.log(typeof name);
+
+var name="goyal"; //redeclaring the variable
+
+// let age=20;
+// console.log(age);
+
+const c="india";
+
+console.log(c);
+
+//datatype 
+var name="vansh";
+var age=20;
+var isStudent=true;
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(isStudent));
+
+var a;
+console.log(a); //undefined
+var object={name:"vansh",age:20};
+console.log(object);
+console.log(typeof(object));
+
+var a=10;
+var b=20;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
+console.log(a==b);
